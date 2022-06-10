69de89da5fcb3b3e032ae21467d2ed8cfe9dea7f
badjateamisha <amishabadjate175@gmail.com>
2022-06-10 08:35:21 +0530

    Creating a table in database

--- AddressBookDatabase.sql
+++ AddressBookDatabase.sql
@@ -1,2 +1,15 @@
 create Database AddressBookService;
 use AddressBookService;
+
+create table AddressBook
+(
+ID int Primary Key Identity (1,1),
+FirstName Varchar(100),
+LastName Varchar(100),
+Address Varchar(100),
+City Varchar(100),
+State Varchar(100),
+Zip int,
+PhoneNumber Varchar(100),
+Email_ID Varchar(100)
+);
